f7754a8d0f0a12f52a2291ff197620808f4b9b88
szabika-pvp <szatomi2008@gmail.com>
2026-02-11 11:36:15 +0100

    initial commit

---
+++ src/main/java/hu/szatomi/lifesteal/Lifesteal.java
@@ -0,0 +1,17 @@
+package hu.szatomi.lifesteal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lifesteal extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
